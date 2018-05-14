2c1b0752e8cac3fd7c4caa5f95bc6976fd628a14
nathan <nhamard@et.esiea.fr>
2018-05-14 13:32:17 +0200

    Petite mise à jour

---
+++ src/RCLAB/UserBundle/Resources/public/js/scripts.js
@@ -0,0 +1,35 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("../images/registration.jpg");
+    $('#header-bar').on('shown.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    $('#header-bar').on('hidden.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    
+    /*
+        Form validation
+    */
+    $('[type="text"], [type="password"], [type="email"], textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('[type="text"], [type="password"], [type="email"], textarea').each(function(){
+    		if( $(this).val() === "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+});
