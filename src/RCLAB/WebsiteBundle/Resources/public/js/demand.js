0d38a5006848cd06c388d7c0adcb4d33a59594aa
nathan <nhamard@et.esiea.fr>
2018-06-11 11:25:56 +0200

    PAGE DE COURS PRETE + PAGE DES REPARATIONS PRETE

---
+++ src/RCLAB/WebsiteBundle/Resources/public/js/demand.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $.backstretch($("course-body").attr('href'));
+    $('#header-bar').on('shown.bs.collapse', function(){
+        $.backstretch("resize");
+    });
+    $('#header-bar').on('hidden.bs.collapse', function(){
+        $.backstretch("resize");
+    });
+
+    $('.alert').delay(4000).fadeOut(2000);
+
+});
